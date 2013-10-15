93f7feed1a65b52ff6c40c2296bdbf668c9bd35f
Mikel <mikel@zhenit.com>
2013-10-15 15:36:18 +0200

    Comentar operativa bank store

--- app/code/local/Mage/PayTpvCom/Model/System/Config/Source/Operativas.php
+++ app/code/local/Mage/PayTpvCom/Model/System/Config/Source/Operativas.php
@@ -4,7 +4,7 @@ class Mage_PayTpvCom_Model_System_Config_Source_Operativas {
 		return array(
             array('value'=>0, 'label'=>Mage::helper('adminhtml')->__('Offsite')),
             array('value'=>1, 'label'=>Mage::helper('adminhtml')->__('Iframe')),
-            array('value'=>1, 'label'=>Mage::helper('adminhtml')->__('BankStore'))
+//            array('value'=>2, 'label'=>Mage::helper('adminhtml')->__('BankStore'))
 		);
 	}
 }
