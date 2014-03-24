931f117eccf26e7671e7dd8072ca359bff370eb1
Mikel Martin <mikel@zhenit.com>
2014-03-24 23:18:42 +0100

    [FIX] Cmopatibility issue with PHP < 5.4

--- app/code/community/Mage/PayTpvCom/Model/System/Config/Source/Integraciones.php
+++ app/code/community/Mage/PayTpvCom/Model/System/Config/Source/Integraciones.php
@@ -3,9 +3,9 @@ class Mage_PayTpvCom_Model_System_Config_Source_Integraciones
 {
     public function toOptionArray()
     {
-        return [
-            ['value'=>0, 'label'=>Mage::helper('adminhtml')->__('OFFSITE')],
-            ['value'=>1, 'label'=>Mage::helper('adminhtml')->__('IFRAME')]
-        ];
+        return array(
+            array('value'=>0, 'label'=>Mage::helper('adminhtml')->__('OFFSITE')),
+            array('value'=>1, 'label'=>Mage::helper('adminhtml')->__('IFRAME'))
+        );
     }
 }
