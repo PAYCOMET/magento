91428b540c31f88372daafcb9f0216e4e1dbd4af
Mikel Martin <mikel@zhenit.com>
2014-11-13 10:27:45 +0100

    Added missing upgrade file

---
+++ app/code/community/Mage/PayTpvCom/sql/paytpvcom_setup/mysql4-upgrade-3.1.1-3.1.2.php
@@ -0,0 +1,23 @@
+<?php
+$installer = $this;
+
+$installer->startSetup();
+$setup = Mage::getModel('customer/entity_setup', 'core_setup');
+$setup->addAttribute('customer', 'paytpv_recall', array(
+	'type' => 'int',
+	'global' => 1,
+	'visible' => 0,
+	'required' => 0,
+	'user_defined' => 0,
+	'default' => '1',
+	'visible_on_front' => 0
+));
+$tableorder = $this->getTable('sales/order');
+$installer->run("
+ALTER TABLE  $tableorder
+    ADD `paytpv_card_country_iso3` VARCHAR( 3 ) NULL DEFAULT NULL,
+    ADD `paytpv_card_type` VARCHAR( 32 ) NULL DEFAULT NULL,
+    ADD `paytpv_card_brand` VARCHAR( 32 ) NULL DEFAULT NULL ;
+");
+
+$installer->endSetup();
